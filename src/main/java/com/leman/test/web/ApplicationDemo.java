d236bff6af6ba9df4f82e526236c0a80ace6551f
chenshouhong <641673461@qq.com>
2017-10-30 20:55:04 +0800

    尝试使用git

---
+++ src/main/java/com/leman/test/web/ApplicationDemo.java
@@ -0,0 +1,18 @@
+package com.leman.test.web;
+
+import java.util.HashMap;
+
+/**
+ * Created by chenshouhong on 2017/7/17.
+ */
+
+public class ApplicationDemo {
+
+    public static void main(String[] args) throws Exception {
+       System.out.println(" springApplication run !");
+     //   SpringApplication.run(ApplicationDemo.class, args);
+        HashMap map = new HashMap<String,Object>();
+
+        System.out.println(1234);
+    }
+}
